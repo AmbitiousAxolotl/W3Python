391e1623bae9809252107e00b4f2566988f8edee
Jermal L Brooks <79384045+AmbitiousAxolotl@users.noreply.github.com>
2024-08-21 16:20:39 -0500

    General learning starting with variables and numbers

---
+++ W3Learning.py
@@ -0,0 +1,15 @@
+myVariableName = "Ax0Assistant"
+
+x,y,z = "Assisting","Tasks","Improving Work";
+print(x);
+print(y);
+print(z);
+
+
+
+print("What is your Age?");
+#testing my python knowledge so i can include this is my mastering. 
+if y == int(23):
+    print(y);
+
+    elif print(0);
