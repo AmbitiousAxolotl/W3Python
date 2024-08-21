915028c7a06af40471c245dd5a20c1ced376e0a4
Jermal L Brooks <79384045+AmbitiousAxolotl@users.noreply.github.com>
2024-08-21 16:51:33 -0500

    going over the basics again when it comes to vairables and outputs

--- W3Learning.py
+++ W3Learning.py
@@ -1,6 +1,7 @@
 myVariableName = "Ax0Assistant"
 
-x,y,z = "Assisting","Tasks","Improving Work";
+Features =["Assisting ","Tasks","Improving Work"]
+x,y,z = Features
 print(x);
 print(y);
 print(z);
@@ -9,7 +10,5 @@ print(z);
 
 print("What is your Age?");
 #testing my python knowledge so i can include this is my mastering. 
-if y == int(23):
-    print(y);
-
-    elif print(0);
+y == int(23);
+print(y);
