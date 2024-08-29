e7b525c56fef5a5b07f5871186b697e1ff723679
Jermal L Brooks <79384045+AmbitiousAxolotl@users.noreply.github.com>
2024-08-29 09:52:42 -0500

    Hangman Project

--- W3Learning.py
+++ W3Learning.py
@@ -1,4 +1,4 @@
-myVariableName = "Ax0Assistant" #Variable Name
+AxoBot = "Ax0Assistant" #Variable Name
 
 Features =["Assisting ","Tasks","Improving Work"] #lists
 x,y,z = Features
@@ -10,5 +10,28 @@ print(z);
 
 print("What is your Age?");
 #testing my python knowledge so i can include this is my mastering. 
+age = 23
+
+txt = "My name is Jermy I am {age}"
+
+print(txt)
+
+
 y == int(23); #Variable
 print(y);
+
+for x in "Machine Learning":
+    print(x);
+
+
+print(len(x));
+
+txt = "StarField is our near future."
+
+if "StarField" in txt: print("Cube Food!")
+
+txt = "Life is all about Happiness, Skills, Knowledge, Luck"
+if "Unfair" not in txt:
+    print("Sometimes life is unfair but keep push you will get there.")
+
+    
