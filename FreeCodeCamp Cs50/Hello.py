f42e8a70a5754f031146c46134ba3dfcc4ff72a2
Jermal L Brooks <79384045+AmbitiousAxolotl@users.noreply.github.com>
2024-09-11 12:00:43 -0500

    Added A freecode camp section for the harvard video

--- FreeCodeCamp Cs50/Hello.py
+++ FreeCodeCamp Cs50/Hello.py
@@ -1,7 +1,9 @@
 #Outputs username that follows up with the password for the application
-Username = input("Username PLEASE " + Username)
+Username = input("Username PLEASE ")
 print(Username)
 
 #Outputs the password for the user
 Password = input("Password ")
 print(Password)
+
+os.path.exsists(file_path)
